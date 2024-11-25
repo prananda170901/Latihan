a6da71053a919320db8d6505f350a76330a9876e
PranandaAditya <prananda17092001@gmail.com>
2024-11-26 01:43:27 +0700

    ubah

--- latihan.py
+++ latihan.py
@@ -1,3 +1,6 @@
+#Belajar menulis komentar pada Python
+print("Hello World")
 print("Aku Siap")
 print("Belajar Analisis Data Dengan Python")
 print("Dengan Semangat")
+#Fungsi print () digunakan untuk mencetak statement
